dc67eba9e97dbb8c688f01e31fff7e5822bf6848
shkim <sh.kim@knu.ac.kr>
2026-05-08 12:12:38 +0900

    Remove workflow module preload alias

--- ai_mesh_generator/amg/workflow/__init__.py
+++ ai_mesh_generator/amg/workflow/__init__.py
@@ -1,8 +1,3 @@
-"""Primary AMG end-to-end workflows."""
+"""Primary AMG end-to-end workflow package."""
 
-from ai_mesh_generator.amg.workflow.entity_size_field_gate import (
-    EntitySizeFieldWorkflowError,
-    run_entity_size_field_gate_workflow,
-)
-
-__all__ = ["EntitySizeFieldWorkflowError", "run_entity_size_field_gate_workflow"]
+__all__: list[str] = []
